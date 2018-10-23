947c37edf2c9ecc9ddb0be14b49c6fffc34defcc
WonSoo Suh <inetsos@naver.com>
2018-10-23 21:50:01 +0900

    restart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    Error404Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+    _id: string;
+    username: string;
+    password: string;
+    name: string;
+    storeName: string;
+    phone: string;
+    email: string;
+    confirmPassword: string;
+    currentPassword: string;
+    newPassword: string;
+}
